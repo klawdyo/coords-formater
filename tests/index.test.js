9541fa7a61e1b4230debb1eb77832baa5a48ead3
Cláudio Medeiros <klawdyo@gmail.com>
2020-01-17 16:14:25 -0300

    add toFloat() tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,4 @@
-require('./normalize.test')
-require('./parse.test')
-require('./convert.test')
+// require('./normalize.test')
+// require('./parse.test')
+// require('./convert.test')
+require('./toFloat.test')
