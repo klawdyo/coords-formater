0567db5bef93b0ee95e495ff6a168c0d218a8b01
Cláudio Medeiros <klawdyo@gmail.com>
2024-09-27 17:18:51 -0300

    fix: finaliza o normalize

--- src/normalizer.spec.ts
+++ src/normalizer.spec.ts
@@ -1,83 +1,124 @@
+import { Normalizer } from './normalizer';
+
+/* eslint-disable no-misleading-character-class */
 describe('Normalizer', () => {
-  describe('Normalizer', () => {
+  describe('normalize', () => {
     test('Normalize verify', () => {
       expect(`41° 25' 01" W`).toBe(`41° 25' 01" W`);
     });
   });
 
-  describe('Normalize verifying degrees indicators: ( ° º o ° ˚ ̊  ⁰ ∘ ◦ ॰)', () => {
-    test.each([
-      `41° 25' 01" W`,
-      `41º 25' 01" W`,
-      `41o 25' 01" W`,
-      `41° 25' 01" W`,
-      `41˚ 25' 01" W`,
-      `41̊ 25' 01" W`,
-      `41⁰ 25' 01" W`,
-      `41∘ 25' 01" W`,
-      `41◦ 25' 01" W`,
-      `41॰ 25' 01" W`,
-    ])('Normalize verify', (data) => {
-      expect(data).toBe(`41° 25' 01" W`);
+  describe('Unicode', () => {
+    const degreeIndicators = [
+      0x00b0, // ° -  U+00B0 (DEGREE SIGN)                 <--------- Caractere padrão
+      0x00ba, // º -  U+00BA (MASCULINE ORDINAL INDICATOR)
+      0x006f, // o -  U+006F (LATIN SMALL LETTER O)
+      0x02da, // ˚ -  U+02DA (RING ABOVE)
+      // 0x030a, // ̊ -   U+030A (COMBINING RING ABOVE) <-- Só existe em combinação com outro
+      0x2070, // ⁰ -  U+2070 (SUPERSCRIPT ZERO)
+      0x2218, // ∘ -  U+2218 (RING OPERATOR)
+      0x2219, // ◦ -  U+2219 (BULLET OPERATOR)
+      0x0968, // ॰ -  U+0968 (DEVANAGARI SIGN AVAGRAHA)
+      0x25e6, // ॰ -  U+25E6 (WHITE CIRCLE)
+      0x0970, // ० -  U+0970 (DEVANAGARI DIGIT ZERO)
+    ];
+
+    const singleQuotes = [
+      0x0027, // ' -  U+0027 (Acento agudo)              <--------- Caractere padrão
+      0x02b9, // ʹ -  U+02B9 (Acento agudo modificador)
+      0x02bc, // ʼ -  U+02BC (Acento agudo modificador)
+      0x02c8, // ˈ -  U+02C8 (Acento agudo modificador)
+      0x05f3, // ׳ -  U+05F3 (Acento agudo hebraico)
+      0x2032, // ′ -  U+2032 (Prime)
+      0xa78c, // ꞌ -  U+A78C (Acento agudo latino)
+    ];
+
+    const doubleQuotes = [
+      0x0022, // "  - U+0022 (QUOTATION MARK)             <------- Caractere padrão de segundos
+      0x201e, // „  - U+201E (DOUBLE LOW-9 QUOTATION MARK)
+      0x201c, // “  - U+201C (LEFT DOUBLE QUOTATION MARK)
+      0x201d, // ”  - U+201D (RIGHT DOUBLE QUOTATION MARK)
+      0x301d, // 〝 - U+301D (LEFT WHITE DOUBLE QUOTATION MARK)
+      0x301e, // 〞 - U+301E (RIGHT WHITE DOUBLE QUOTATION MARK)
+      0x201f, // “  - U+201F (DOUBLE HIGH-REVERSED-9 QUOTATION MARK)
+    ];
+
+    test.each(degreeIndicators)('Testa a substituição de %s do indicator de graus', (indicator) => {
+      const coord = `41${String.fromCodePoint(indicator)} 25' 01" W`;
+      const replaced = coord.replace(/[\u00BA\u006F\u02DA\u030A\u2070\u2218\u2219\u0968\u25E6\u0970]/gu, String.fromCodePoint(0x00b0));
+      expect(replaced).toBe(`41° 25' 01" W`);
+      expect(Normalizer.normalize(coord)).toBe(`41° 25' 01" W`);
+    });
+
+    test.each(singleQuotes)('Testa a substituição de %s do indicator de minutos', (indicator) => {
+      const coord = `41° 25${String.fromCodePoint(indicator)} 01" W`;
+      const replaced = coord.replace(/[\u02B9\u02BC\u02C8\u05F3\u2032\uA78C]/gu, String.fromCodePoint(0x0027));
+      expect(replaced).toBe(`41° 25' 01" W`);
+      expect(Normalizer.normalize(coord)).toBe(`41° 25' 01" W`);
+    });
+
+    test.each(doubleQuotes)('Testa a substituição de %s do indicator de segundos', (indicator) => {
+      const coord = `41° 25' 01${String.fromCodePoint(indicator)} W`;
+      const replaced = coord.replace(/[\u201E\u201C\u201D\u301D\u301E\u201F]/gu, String.fromCodePoint(0x0022));
+      expect(replaced).toBe(`41° 25' 01" W`);
+      expect(Normalizer.normalize(coord)).toBe(`41° 25' 01" W`);
+    });
+  });
+
+  describe('Strings', () => {
+    const degreeIndicators = [
+      '°', // ° - U+00B0 (DEGREE SIGN)                 <--------- Caractere padrão
+      'º', // º - U+00BA (MASCULINE ORDINAL INDICATOR)
+      'o', // o - U+006F (LATIN SMALL LETTER O)
+      '˚', // ˚ - U+02DA (RING ABOVE)
+      '⁰', // ⁰ - U+2070 (SUPERSCRIPT ZERO)
+      '∘', // ∘ - U+2218 (RING OPERATOR)
+      '॰', // ॰ -  U+25E6 (WHITE CIRCLE)
+      // '०', // ० -  U+0970 (DEVANAGARI DIGIT ZERO)  <-- não funciona na substituição da string
+      // '̊', // ̊ - U+030A (COMBINING RING ABOVE) <-- Só funciona combinado com outro
+      // '◦', // ◦ - U+2219 (BULLET OPERATOR) <-- não funciona na substituição da string
+      // '॰', // ॰ - U+0968 (DEVANAGARI SIGN AVAGRAHA) <-- não funciona na substituição da string
+    ];
+
+    const singleQuotes = [
+      "'", // ' - U+0027 (Acento agudo)              <--------- Caractere padrão
+      'ʹ', // ʹ - U+02B9 (Acento agudo modificador)
+      'ʼ', // ʼ - U+02BC (Acento agudo modificador)
+      'ˈ', // ˈ - U+02C8 (Acento agudo modificador)
+      '׳', // ׳ - U+05F3 (Acento agudo hebraico)
+      '′', // ′ - U+2032 (Prime)
+      'ꞌ', // ꞌ - U+A78C (Acento agudo latino)
+    ];
+
+    const doubleQuotes = [
+      '"', // "  - U+0022 (QUOTATION MARK)             <------- Caractere padrão de segundos
+      '„', // „  - U+201E (DOUBLE LOW-9 QUOTATION MARK)
+      '“', // “  - U+201C (LEFT DOUBLE QUOTATION MARK)
+      '”', // ”  - U+201D (RIGHT DOUBLE QUOTATION MARK)
+      '〝', // 〝 - U+301D (LEFT WHITE DOUBLE QUOTATION MARK)
+      '〞', // 〞 - U+301E (RIGHT WHITE DOUBLE QUOTATION MARK)
+      '“', // “  - U+201F (DOUBLE HIGH-REVERSED-9 QUOTATION MARK)
+    ];
+
+    test.each(degreeIndicators)('Testa a substituição de %s do indicator de graus', (indicator) => {
+      const coord = `41${indicator} 25' 01" W`;
+      const replaced = coord.replace(/[\u00BA\u006F\u02DA\u030A\u2070\u2218\u2219\u0968\u25E6\u0970]/gu, String.fromCodePoint(0x00b0));
+      expect(replaced).toBe(`41° 25' 01" W`);
+      expect(Normalizer.normalize(coord)).toBe(`41° 25' 01" W`);
+    });
+
+    test.each(singleQuotes)('Testa a substituição de %s do indicator de minutos', (indicator) => {
+      const coord = `41° 25${indicator} 01" W`;
+      const replaced = coord.replace(/[\u02B9\u02BC\u02C8\u05F3\u2032\uA78C]/gu, String.fromCodePoint(0x0027));
+      expect(replaced).toBe(`41° 25' 01" W`);
+      expect(Normalizer.normalize(coord)).toBe(`41° 25' 01" W`);
+    });
+
+    test.each(doubleQuotes)('Testa a substituição de %s do indicator de segundos', (indicator) => {
+      const coord = `41° 25' 01${indicator} W`;
+      const replaced = coord.replace(/[\u201E\u201C\u201D\u301D\u301E\u201F]/gu, String.fromCodePoint(0x0022));
+      expect(replaced).toBe(`41° 25' 01" W`);
+      expect(Normalizer.normalize(coord)).toBe(`41° 25' 01" W`);
     });
   });
 });
-
-describe.only('Unicode', () => {
-  describe('graus', () => {
-    /*
-    test('Vrifica o unicode', () => {
-        ['°', 'º', 'o', '˚', '̊', '⁰', '∘', '◦', '॰'].map(item => item.)
-    })    
-        °: U+00B0
-        º: U+00BA, // <--- padrão. é o Alt+167
-        o: U+006F
-        ˚: U+02DA
-        ̊: U+030A
-        ⁰: U+2070
-        ∘: U+2218
-        ◦: U+25E6
-        ॰: U+0970
-    */
-    test('', () => {
-      const replaced = `41° 25' 01" W`.replace(/\u{00B0}|\u{00BA}|\u{006F}|\u{02DA}|\u{030A}|\u{2070}|\u{2218}|\u{25E6}|\u{0970}/ug, '°');
-      console.log(replaced);
-      
-    });
-  });
-});
-
-// //
-// test('Normalize verifying degrees indicators: (° º o ° ˚ ̊  ⁰ ∘ ◦ ॰)', t => {
-//   t.assert(normalize(`41° 25' 01" W`) === `41° 25' 01" W`, 'Normal normalize')
-//   t.assert(normalize(`41o 25' 01" W`) === `41° 25' 01" W`, '"o" as degree indicator')
-//   t.assert(normalize(`41º 25' 01" W`) === `41° 25' 01" W`, '"º" as degree indicator')
-//   t.assert(normalize(`41º 25' 01" W`) === `41° 25' 01" W`, '"°" as degree indicator')
-//   t.assert(normalize(`41º 25' 01" W`) === `41° 25' 01" W`, '"˚" as degree indicator')
-//   t.assert(normalize(`41º 25' 01" W`) === `41° 25' 01" W`, '"⁰" as degree indicator')
-//   t.assert(normalize(`41º 25' 01" W`) === `41° 25' 01" W`, '"∘" as degree indicator')
-//   t.assert(normalize(`41º 25' 01" W`) === `41° 25' 01" W`, '"◦" as degree indicator')
-//   t.assert(normalize(`41º 25' 01" W`) === `41° 25' 01" W`, '"॰" as degree indicator')
-
-//   t.end()
-// })
-
-// test("Normalize verifying minutes indicators: ('ʹʼˈ׳′ꞌ)   ", t => {
-//   t.assert(normalize('41° 25` 01" W') === `41° 25' 01" W`, '` as minutes indicator')
-//   t.assert(normalize(`41° 25' 01" W`) === `41° 25' 01" W`, `' as minutes indicator`)
-
-//   t.assert(normalize(`41° 25ʼ 01" W`) === `41° 25' 01" W`, `ʼ as minutes indicator`)
-//   t.assert(normalize(`41° 25ˈ 01" W`) === `41° 25' 01" W`, `ˈ as minutes indicator`)
-//   t.assert(normalize(`41° 25′ 01" W`) === `41° 25' 01" W`, `′ as minutes indicator`)
-//   t.assert(normalize(`41° 25ꞌ 01" W`) === `41° 25' 01" W`, `ꞌ as minutes indicator`)
-
-//   t.end()
-// })
-
-// test("Normalize verifying seconds indicators", t => {
-//   t.assert(normalize(`41° 25' 01" W`) === `41° 25' 01" W`, `" as seconds indicator`)
-//   t.assert(normalize(`41° 25' 01'' W`) === `41° 25' 01" W`, `'' (two single quote) as seconds indicator`)
-//   t.assert(normalize(`41° 25' 01ʼʼ W`) === `41° 25' 01" W`, `ʼʼ (two single quote) as seconds indicator`)
-
-//   t.end()
-// })
--- src/normalizer.ts
+++ src/normalizer.ts
@@ -1,17 +1,23 @@
 /* eslint-disable no-misleading-character-class */
 export class Normalizer {
+  /**
+   * Remove tudo o que não for número e ponto
+   * 
+   */
   static clearPart(value?: string): number {
     return +(value?.replace(/[^\d.]/, '') || 0);
   }
 
+  /**
+   * Substitui os caracteres parecidos com aspas simples, duplas e indicador de graus por um valor padrão
+   * 
+   */
   static normalize(value: string): string {
     const normalized = value
       .replace(/\s{2,}/g, ' ') // double spaces
-      .replace(/[°ºo°˚̊⁰∘◦॰]+/g, '°') // degree indicators
-      .replace(/[`'ʹʼˈ׳′ꞌ]{1}/g, "'") // single quotes
-      .replace("''", '"') //two single quotes
-      .replace(/["„“”]+/g, '"') // double quote
-      .replace(/,+/g, '.'); //commas to dots
+      .replace(/[\u00BA\u006F\u02DA\u030A\u2070\u2218\u2219\u0968\u25E6\u0970]/gu, String.fromCodePoint(0x00b0)) // degree indicators
+      .replace(/[\u02B9\u02BC\u02C8\u05F3\u2032\uA78C]/gu, String.fromCodePoint(0x0027)) // single quotes
+      .replace(/[\u201E\u201C\u201D\u301D\u301E\u201F]/gu, String.fromCodePoint(0x0022)); // double quotes
 
     return normalized;
   }
